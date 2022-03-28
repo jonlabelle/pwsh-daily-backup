d958910c7211e0f8d7195df5ee00f47aee1f23d5
Jon LaBelle <contact@jonlabelle.com>
2022-03-28 09:52:05 -0500

    Tweaks and updates

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -1,12 +1,18 @@
 $testDir = Split-Path $script:MyInvocation.MyCommand.Path
 
 $projectRootDir = (Get-Item $testDir).Parent.FullName
+
+$moduleName = "Backup-File"
+$modulePath = (Join-Path "$projectRootDir" "${moduleName}.ps1")
+if ($null -eq (Get-Module -ListAvailable -Name $moduleName))
+{
+    Write-Output "Removing module: $moduleName"
+    Remove-Module $moduleName
+}
 Import-Module (Join-Path "$projectRootDir" "Backup-File.ps1")
 
 $sources = (Join-Path "$projectRootDir" "test" "stubs" "files-to-backup")
 $destination = (Join-Path "$projectRootDir" "test" "stubs" "files-backed-up")
-
 $dryRun = $false
 $verboseEnabled = $true
-
-Backup-File -Path $sources -Destination $destination -DeleteBackupsOldThanDays 7 -WhatIf:$dryRun -Verbose:$verboseEnabled
+Backup-File -Path $sources -Destination $destination -DeleteBackupsOlderThanDays 7 -WhatIf:$dryRun -Verbose:$verboseEnabled
