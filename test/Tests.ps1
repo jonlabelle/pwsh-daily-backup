419bbffbdaac38a9f47a10d0293e079a93d8b108
Jon LaBelle <contact@jonlabelle.com>
2022-03-30 19:10:13 -0500

    Sort folders by date

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -7,7 +7,7 @@ $modulePath = (Join-Path -Path "$projectRootDir" -ChildPath "$moduleName.ps1")
 $dryRun = $false
 $verboseEnabled = $true
 
-if (-not ($null -eq (Get-ChildItem -Path Function:\$moduleName)))
+if (-not ($null -eq (Get-ChildItem -Path Function:\$moduleName -ErrorAction 'SilentlyContinue')))
 {
     Write-Verbose ("Removing old {0} from memory" -f "Function:\$moduleName") -Verbose:$verboseEnabled
 
@@ -17,7 +17,7 @@ if (-not ($null -eq (Get-ChildItem -Path Function:\$moduleName)))
     }
     catch
     {
-        Write-Warning -Message "An error occured attempting to remove Function:\$moduleName from memory"
+        Write-Warning -Message "An error occured attempting to remove Function:\$moduleName from memory" -Verbose:$verboseEnabled
     }
 }
 
@@ -28,4 +28,4 @@ $sources = (Join-Path "$projectRootDir" "test" "stubs" "files-to-backup")
 $destination = (Join-Path "$projectRootDir" "test" "stubs" "files-backed-up")
 
 Write-Verbose ("Calling {0} cmdlet" -f "$moduleName") -Verbose:$verboseEnabled
-Backup-File -Path $sources -Destination $destination -DeleteBackupsOlderThanDays 7 -WhatIf:$dryRun -Verbose:$verboseEnabled
+Backup-File -Path $sources -Destination $destination -DailyBackupsToKeep 2 -WhatIf:$dryRun -Verbose:$verboseEnabled
