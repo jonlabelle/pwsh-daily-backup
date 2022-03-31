33793859f7c090898fb3a68eea514b36f87b7747
Jon LaBelle <contact@jonlabelle.com>
2022-03-30 20:56:26 -0500

    Update bash test script runner

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -7,4 +7,27 @@ set -o pipefail
 SCRIPTSDIR="$(cd "$(dirname "${0}")" || exit; echo "$(pwd)")"
 cd "${SCRIPTSDIR}/.." || exit 1
 
-/usr/local/bin/pwsh -NonInteractive -NoProfile -NoLogo -ExecutionPolicy ByPass -File test/Tests.ps1
+run_test_script() {
+    echo "----------------------------"
+    echo " Running test script"
+    echo "----------------------------"
+    echo ""
+    /usr/local/bin/pwsh -NonInteractive -NoProfile -NoLogo -ExecutionPolicy ByPass -File test/Tests.ps1
+    echo ""
+    echo "Finished running test script."
+    echo ""
+}
+
+run_test_command() {
+    echo "----------------------------"
+    echo " Running test command"
+    echo "----------------------------"
+    echo ""
+    /usr/local/bin/pwsh -NonInteractive -NoProfile -NoLogo -ExecutionPolicy ByPass -Command '. ./Backup-File.ps1; Backup-File -Path ".github" -Destination "tmp2/test" -DailyBackupsToKeep 2 -WhatIf -Verbose'
+    echo ""
+    echo "Finished running test command."
+    echo ""
+}
+
+run_test_script
+run_test_command
