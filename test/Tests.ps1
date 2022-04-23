126d432c4abac3f0ead92196ea1fe284e4ed1a8a
Jon LaBelle <contact@jonlabelle.com>
2022-04-23 15:05:14 -0500

    Fix Join-Path Cannot find path

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -16,9 +16,5 @@ $path1 = (Join-Path -Path "$projectRootDir" -ChildPath 'test' -AdditionalChildPa
 $path2 = (Join-Path -Path "$projectRootDir" -ChildPath '.github')
 $destination = (Join-Path -Path "$projectRootDir" -ChildPath 'test' -AdditionalChildPath 'stubs', 'files-backed-up')
 
-$path1 = Resolve-Path -Path $path1
-$path2 = Resolve-Path -Path $path2
-$destination = Resolve-Path -Path $destination
-
 Write-Verbose ('Running: {0}' -f "$moduleName") -Verbose:$verboseEnabled
 Backup-File -Path $path1, $path2 -Destination $destination -DailyBackupsToKeep 2 -WhatIf:$WhatIf -Verbose:$Verbose
