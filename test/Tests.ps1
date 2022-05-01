6d87f0128515692258f5d7ad173a5da3b70e00e5
Jon LaBelle <contact@jonlabelle.com>
2022-05-01 11:30:37 -0500

    Fix whatif, verbose

--- test/Tests.ps1
+++ test/Tests.ps1
@@ -5,16 +5,34 @@ param(
     $WhatIf = $false
 )
 
+$dryRun = $false
+if ($WhatIf)
+{
+    $dryRun = $true
+}
+
+$verboseEnabled = $false
+if ($VerbosePreference -eq 'Continue')
+{
+    $verboseEnabled = $true
+}
+
 $projectRootDir = (Join-Path -Path $PSScriptRoot -ChildPath '..')
+
 $moduleName = 'DailyBackup'
 $modulePath = (Join-Path -Path $PSScriptRoot -ChildPath '..' -AdditionalChildPath $moduleName)
 
-Get-Module $moduleName | Remove-Module -Verbose -ErrorAction SilentlyContinue
-Import-Module -Name "$modulePath" -Force -Verbose
+Get-Module $moduleName | Remove-Module -Verbose:$verboseEnabled -ErrorAction SilentlyContinue
+Import-Module -Name "$modulePath" -Force -Verbose:$verboseEnabled
 
 $path1 = (Join-Path -Path "$projectRootDir" -ChildPath 'test' -AdditionalChildPath 'stubs', 'files-to-backup')
 $path2 = (Join-Path -Path "$projectRootDir" -ChildPath '.github')
 $destination = (Join-Path -Path "$projectRootDir" -ChildPath 'test' -AdditionalChildPath 'stubs', 'files-backed-up')
 
 Write-Verbose ('Running: {0}' -f "$moduleName") -Verbose:$verboseEnabled
-New-DailyBackup -Path $path1, $path2 -Destination $destination -DailyBackupsToKeep 2 -WhatIf:$WhatIf -Verbose:$Verbose
+New-DailyBackup `
+    -Path $path1, $path2 `
+    -Destination $destination `
+    -DailyBackupsToKeep 2 `
+    -WhatIf:$dryRun `
+    -Verbose:$verboseEnabled
